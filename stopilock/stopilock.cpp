65306a438454229e8ba41ae56fdac7c197bab7af
hjgode <hjgode@gmail.com>
2013-11-11 03:17:45 -0800

    stopilock can stop iLock4 using a predfined event name or by providing a name via cmd line

--- stopilock/stopilock.cpp
+++ stopilock/stopilock.cpp
@@ -7,9 +7,24 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HANDLE hStopIlock = CreateEvent(NULL, FALSE, FALSE, STOPEVENTNAME);
-	if(hStopIlock!=NULL)
-		SetEvent(hStopIlock);
+	HANDLE hStopIlock=NULL;
+	if(argc==1)
+		 hStopIlock = CreateEvent(NULL, FALSE, FALSE, STOPEVENTNAME);
+	else if (argc==2){
+		TCHAR eventName[64];
+		wsprintf(eventName, L"%s", argv[1]);
+		hStopIlock = CreateEvent(NULL, FALSE, FALSE, eventName);
+	}
+
+	DEBUGMSG(1, (L"stopilock: CreateEvent returned: %i\r\n", GetLastError()));
+
+	if(hStopIlock!=NULL){
+		if(SetEvent(hStopIlock))
+			DEBUGMSG(1, (L"stopilock: SetEvent OK, returned: %i\r\n", GetLastError()));
+		else
+			DEBUGMSG(1, (L"stopilock: SetEvent FALSE, returned: %i\r\n", GetLastError()));
+	}
+
 	return 0;
 }
 
