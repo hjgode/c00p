8774be37a6196ad54ec4285694d438c8c0b8afd6
hjgode <hjgode@gmail.com>
2013-12-05 06:43:48 -0800

    updated test dir

--- iTimedReboot2/test/test_bat.bat
+++ iTimedReboot2/test/test_bat.bat
@@ -6,279 +6,186 @@ echo  AVOID AUTOMATIC TIME SYNCS!
 echo .
 PAUSE Press any key to start
 
-copy /Y "..\Windows Mobile 6 Professional SDK (ARMV4I)\Debug\iTimedReboot2.exe" .
-
-ECHO ############## START ############### >test_run.txt
-
-echo Importing test reg keys...
-echo Importing test reg keys... >>test_run.txt
-pregutl @iTimedReboot2.reg >>test_run.txt
-
-REM the reg should now contain
-rem LastBootDate=20131129
-rem RebootDays=0
-rem RebootTime=12:45
+REM copy actual debug binary to test dir
+echo "Copy of actual binary..."
+copy /Y "..\Windows Mobile 6 Professional SDK (ARMV4I)\Debug\iTimedReboot2.exe" . >NUL
+echo "...done"
 
 rem Copy files...
+echo "Copy tools..."
 pdel \iTimedReboot2.exe.log.txt >NUL
 pput -f ./iTimedReboot2.exe \iTimedReboot2.exe
 pdel \setDateTime.exe.log.txt >NUL
 pput -f ./setDateTime.exe \setDateTime.exe
+echo "...done"
 
-ECHO TEST1...
-ECHO 	set time manually to 29.11.2013 11:00
-echo 	run iTimedReboot2.exe
-ECHO ++++++++++++++ TEST1 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-ECHO set time manually to 01.01.2013 11:00 >>test_run.txt
-echo    simulate a iTimedReboot2 call before valid date >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201311291100
-CALL :MYWAIT
+REM clear local test log file
+ECHO ############## START ############### >test_run.txt
+
+REM TEST1
+call:mylog "======================================================"
+call:mylog " TEST1..."
+call:mylog "    test systemzeit vor rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 10.11.2013
+rem setze system zeit auf 14:00
+SET systime=201311101400
+call:mysetdatetime %systime%
+rem setze daysinterval auf 1
+set mydayinterval=1
+rem setze lastrebootdate auf 10.11.2013, next reboot waere dann am 11.11.2013 15:00
+set mylastreboot=20131110
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 1
+                 
+REM TEST2
+call:mylog "======================================================"
+call:mylog " TEST2..."
+call:mylog "    test systemzeit nach rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 11.11.2013
+rem setze system zeit auf 16:00
+SET systime=201311111600
+call:mysetdatetime %systime%
+rem setze daysinterval auf 1
+set mydayinterval=1
+rem setze lastrebootdate auf 10.11.2013, next reboot waere dann am 11.11.2013 15:00
+set mylastreboot=20131110
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 2
+
+REM TEST3
+call:mylog "======================================================"
+call:mylog " TEST3..."
+call:mylog "    test systemzeit zu rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 11.11.2013
+rem setze system zeit auf 15:00
+SET systime=201311111500
+call:mysetdatetime %systime%
+rem setze daysinterval auf 1
+set mydayinterval=1
+rem setze lastrebootdate auf 10.11.2013, next reboot waere dann am 11.11.2013 15:00
+set mylastreboot=20131110
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 3
+
+REM TEST4
+call:mylog "======================================================"
+call:mylog " TEST4..."
+call:mylog "   test systemzeit zu 2 minuten nach rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 11.11.2013
+rem setze system zeit auf 15:02
+SET systime=201311111502
+call:mysetdatetime %systime%
+rem setze daysinterval auf 1
+set mydayinterval=1
+rem setze lastrebootdate auf 10.11.2013, next reboot waere dann am 11.11.2013 15:00
+set mylastreboot=20131110
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 4
+        
+REM TEST5
+call:mylog "======================================================"
+call:mylog " TEST5..."
+call:mylog "   test systemzeit weit nach letzter rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 11.11.2013
+rem setze system zeit auf 15:00
+SET systime=201311111500
+call:mysetdatetime %systime%
+rem setze daysinterval auf 7
+set mydayinterval=7
+REM 2. april 2006 was a sunday
+rem setze lastrebootdate auf 2.4.2006, next reboot waere dann am Sonntag den 17.11.2013 15:00
+Rem lastreboot sollte dann der 10.11.2013 sein
+set mylastreboot=20060402
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 5
+
+REM TEST5
+call:mylog "======================================================"
+call:mylog " TEST6..."
+call:mylog "   test systemzeit weit vor letzter rebootzeit"
+call:mylog "======================================================"
+rem setze system datum auf 11.11.2014
+rem setze system zeit auf 15:00
+SET systime=201411111500
+call:mysetdatetime %systime%
+rem setze daysinterval auf 7
+set mydayinterval=7
+REM 2. april 2006 was a sunday
+rem setze lastrebootdate auf 2.4.2006, next reboot waere dann am Sonntag den 16.11.2014 15:00
+Rem lastreboot sollte dann der 9.11.2014 sein
+set mylastreboot=20060402
+rem setze reboottime auf 15:00 
+set myreboottime=15:00
+call:mySetRebootDateTimeAndInterval %mylastreboot%, "%myreboottime%", %mydayinterval%
+rem now test this
+call:MYDOTEST 6
+                                     
+goto:weiter
+rem some date time functions and calculations
+set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
+REM date is 05.12.2013
+echo date is %date%
+echo datetime is %mydate%
+set day=%date:~0,2%
+REM time is 10:51:03,27
+echo time is %time%
+set mytime=%time:~0,2%%time:~3,2%
+set /a newtime=%time:~3,2%-5
+echo time is %mytime%, newtime is %newtime%
+set newtime=%mytime:~0,2%:%newtime%
+echo newtime is now %newtime%
+:weiter
+
+pause
+
+goto:END_TESTS
+
+:MYDOTEST
+REM call with call:MYDOTEST 1
+REM start iTimedReboot2
 prun \iTimedReboot2.exe -test
-ECHO "############# Result:" >>test_run.txt
+call:mylog "Result:"
+REM show reg entries
+pregutl HKLM\Software\Intermec\iTimedReboot2
+REM read back registry into log file
 pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG  TEST1 **************** >>test_run.txt
+call:mylog "*************** LOG  TEST%~1 ****************"
+REM get iTimedReboot2 log file
 pget -f \iTimedReboot2.exe.log.txt
+REM cat logfile to test log
 type iTimedReboot2.exe.log.txt >>test_run.txt
+REM show log
+type iTimedReboot2.exe.log.txt
+REM delete logfile on device
 pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST1  --------------- >>test_run.txt
+REM write marker to test log
+call:mylog "---------------TEST%~1  ---------------"
 ECHO .>>test_run.txt
-
-ECHO TEST2...
-ECHO 	set time manually to 01.01.2013 13:00
-echo    simulate a iTimedReboot2 call after last reboot time
-echo 	run iTimedReboot2.exe
-ECHO ++++++++++++++ TEST2 ++++++++++++++++ >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2  >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-ECHO set time manually to 01.01.2013 13:00 >>test_run.txt
-echo    simulate a iTimedReboot2 call after reboot >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201311291300
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG TEST2**************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST2  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST3...
-ECHO 	set time manually to 29.11.2013 12:45
-ECHO 	run iTimedReboot2.exe 
-ECHO ++++++++++++++ TEST3 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-ECHO set time manually to 01.01.2013 12:00, the time to reboot>>test_run.txt
-ECHO 	iTimedReboot2.exe >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201311291245
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 3 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST3  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST4...
-ECHO 	set time manually to 29.11.2013 12:47
-ECHO 	iTimedReboot2.exe 
-ECHO ++++++++++++++ TEST4 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-ECHO set time manually to 29.11.2013 12:47, within reboot time >>test_run.txt
-ECHO 	iTimedReboot2.exe    >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201311291247
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test 
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG  test 4 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST4  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-GOTO END_TESTS
-
-ECHO TEST5...
-echo 	simulates a scheduler call at correct time
-echo 	set time manually to 02.12.2011 19:55
-echo 	iTimedReboot2.exe -s task2
-ECHO ++++++++++++++ TEST5 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simulates a scheduler call at correct time  >>test_run.txt
-echo set time manually to 02.12.2011 19:55  >>test_run.txt
-echo 	iTimedReboot2.exe -s task2  >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112021955
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test -s task2
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 5 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST5  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST6...
-echo 	simulates a scheduler call at correct time
-echo 	set time manually to 02.12.2011 20:00
-echo 	iTimedReboot2.exe -s task3
-ECHO ++++++++++++++ TEST6 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simulates a scheduler call at correct time  >>test_run.txt
-echo set time manually to 02.12.2011 20:00  >>test_run.txt
-echo 	iTimedReboot2.exe -s task3  >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112022000
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test -s task3
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 6 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST6  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST7...
-echo 	simululates a scheduler call at correct time
-echo 	set time manually to 03.12.2011 0600
-echo 	iTimedReboot2.exe -k task1
-ECHO ++++++++++++++ TEST7 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simulates a scheduler call at correct time  >>test_run.txt
-echo set time manually to 03.12.2011 0600  >>test_run.txt
-echo 	iTimedReboot2.exe -k task1  >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112030600
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test -k task1
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 7 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST7  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST8...
-echo 	simululates a scheduler call at correct time
-echo 	set time manually to 03.12.2011 0605
-echo 	iTimedReboot2.exe -k task2
-
-ECHO ++++++++++++++ TEST8 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simulates a scheduler call at correct time  >>test_run.txt
-echo set time manually to 03.12.2011 0605  >>test_run.txt
-echo 	iTimedReboot2.exe -k task2  >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112030605
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test -k task2
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 8 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST8  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-
-ECHO TEST9...
-echo 	simululates a delayed call
-echo 	set time manually to 04.12.2011 1500
-ECHO 	iTimedReboot2.exe -s task2  				
-ECHO 	iTimedReboot2.exe -s task1  				
-ECHO 	iTimedReboot2.exe -k task1  				
-ECHO 	iTimedReboot2.exe -k task2  				
-echo 	iTimedReboot2.exe   
-ECHO ++++++++++++++ TEST9 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simululates a delayed call  >>test_run.txt
-echo set time manually to 04.12.2011 1500  >>test_run.txt
-ECHO 	iTimedReboot2.exe -s task2  				>>test_run.txt
-ECHO 	iTimedReboot2.exe -s task1  				>>test_run.txt
-ECHO 	iTimedReboot2.exe -k task1  				>>test_run.txt
-ECHO 	iTimedReboot2.exe -k task2  				>>test_run.txt
-echo 	iTimedReboot2.exe   >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112041500
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test -s task2
-prun \iTimedReboot2.exe -test -s task1
-prun \iTimedReboot2.exe -test -k task1
-prun \iTimedReboot2.exe -test -k task2
-prun \iTimedReboot2.exe -test 
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 9 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST9  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST10...
-echo 	simululates a premature TimeChangeEvent
-echo 	set time manually to 04.12.2011 0000
-echo 	iTimedReboot2.exe 
-ECHO ++++++++++++++ TEST10 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simululates a premature TimeChangeEvent  >>test_run.txt
-echo set time manually to 04.12.2011 0000  >>test_run.txt
-echo 	iTimedReboot2.exe   >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112040000
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test 
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 10 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST10  --------------- >>test_run.txt
-ECHO .>>test_run.txt
-
-ECHO TEST11...
-echo 	simululates another premature TimeChangeEvent
-echo 	set time manually to 01.12.2011 0100
-echo 	iTimedReboot2.exe 
-ECHO ++++++++++++++ TEST10 ++++++++++++++++ >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-echo ###	simululates a past time change  >>test_run.txt
-echo set time manually to 01.12.2011 0100  >>test_run.txt
-echo 	iTimedReboot2.exe   >>test_run.txt
-ECHO ------------------------------------- >>test_run.txt
-prun \SetDateTime.exe 201112010100
-CALL :MYWAIT
-prun \iTimedReboot2.exe -test 
-ECHO "############# Result:" >>test_run.txt
-pregutl HKLM\Software\Intermec\iTimedReboot2 >>test_run.txt
-ECHO *************** LOG test 11 **************** >>test_run.txt
-pget -f \iTimedReboot2.exe.log.txt
-type iTimedReboot2.exe.log.txt >>test_run.txt
-pdel \iTimedReboot2.exe.log.txt
-ECHO ---------------TEST11  --------------- >>test_run.txt
-ECHO .>>test_run.txt
+goto:eof
 
 :END_TESTS
-findstr /g:find_start.txt iTimedReboot2.exe.log.txt
+REM this will filter some strings and show matching lines on screen
+REM findstr /g:find_start.txt iTimedReboot2.exe.log.txt
 GOTO MYEND
 
 :MYWAIT
@@ -292,6 +199,43 @@ echo Sleeping 3 sec ...
 PING 1.1.1.1 -n 1 -w 3000 >NUL
 echo ...continue
 GOTO :eof
+
+:mySetRebootDateTimeAndInterval
+rem call with 'call:mySetRebootDateTime 20130512,"15:00",2'
+rem to set date to 12. may 2013  and reboot time to 15:00 and day interval 2
+echo.
+Rem *************************************************************************
+echo ...patch the reg file
+Rem *************************************************************************
+REM replace all !LASTBOOT! with new date
+sed "s/!LASTBOOT!/%~1/ig;s/!REBOOTTIME!/%~2/ig;s/!REBOOTDAYS!/%~3/ig" iTimedReboot2.reg.tmp >iTimedReboot2.reg
+rem sed "s/!REBOOTTIME!/%~2/ig" iTimedReboot2.reg.tmp >iTimedReboot2.reg
+rem sed "s/!REBOOTDAYS!/%~3/ig" iTimedReboot2.reg.tmp >iTimedReboot2.reg
+call:mylog "Importing test reg keys..."
+pregutl @iTimedReboot2.reg >>test_run.txt
+rem to return a value use a var: 
+rem set "var1=DosTips"
+set dt=%~1
+set tt=%~2
+set dt1=%tt% %dt:~6,2%.%dt:~4,2%.%dt:~0,4%
+call:mylog "reboot settings now lastreboot=%dt1%, days interval=%~3"
+goto:eof
+
+:mysetdatetime
+rem call with call:mysetdatetime 201311101400
+prun \SetDateTime.exe %~1
+REM call:mylog "system time now %~1"
+REM show as 14:00 11.10.2013
+set dt=%~1
+set dt1=%dt:~8,2%:%dt:~10,2% %dt:~6,2%.%dt:~4,2%.%dt:~0,4%
+call:mylog "system time now %dt1%"
+goto:eof
+
+:mylog
+rem call with mylog "text to show and log"
+echo %~1
+echo %~1 >>test_run.txt
+goto:eof
 
 :MYEND
 @echo on
