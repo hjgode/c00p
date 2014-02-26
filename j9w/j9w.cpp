26dcfdafa66cf9c307555976c95713927fc860ff
hjgode <hjgode@gmail.com>
2014-02-26 03:31:40 -0800

    another try to commit my changes

---
+++ j9w/j9w.cpp
@@ -0,0 +1,318 @@
+// j9w.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "j9w.h"
+
+#define ILOCK_READY_EVENT L"iLockReady"
+HANDLE hiLockReady=NULL;
+DWORD dwThreadID=NULL;
+HANDLE hThread=NULL;
+
+#define TIMER1 1001
+
+#define MAX_LOADSTRING 100
+#define WM_SHOWDIALOG	WM_USER + 14
+
+//back thread
+DWORD WINAPI watchdogthread(LPVOID lparam){
+	//first sleep 15 seconds
+	HWND hWnd=(HWND) lparam;
+start:
+	Sleep(15000);
+	hiLockReady = CreateEvent(NULL, TRUE, FALSE, ILOCK_READY_EVENT);
+	if(hiLockReady==NULL){
+		//error
+		return -1;
+	}
+	if(GetLastError()!=ERROR_ALREADY_EXISTS){
+		//wrong the handle should already exist
+		CloseHandle(hiLockReady);
+		goto start;
+	}
+	//the handle is valid
+	//show dialog
+	PostMessage(hWnd,WM_SHOWDIALOG,0,0);
+	BOOL bEndThread=FALSE;
+	DWORD dwWaitResult=0;
+	do{
+		dwWaitResult = WaitForSingleObject(hiLockReady, 1000);
+		switch(dwWaitResult){
+			case WAIT_TIMEOUT:
+				break;
+			case WAIT_OBJECT_0:
+				//ilock signaled OK-to-reboot
+				bEndThread=TRUE;
+				break;
+			default:
+				//what?
+				break;
+		}
+	}while(!bEndThread);
+	//we waited and got the reboot signal
+	CloseHandle(hiLockReady);
+	ITCWarmBoot();
+	return 0;
+}
+
+// Global Variables:
+HINSTANCE			g_hInst;			// current instance
+HWND				g_hWndMenuBar;		// menu bar handle
+
+// Forward declarations of functions included in this code module:
+ATOM			MyRegisterClass(HINSTANCE, LPTSTR);
+BOOL			InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPTSTR    lpCmdLine,
+                   int       nCmdShow)
+{
+	MSG msg;
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow)) 
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable;
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_J9W));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0)) 
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) 
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+ATOM MyRegisterClass(HINSTANCE hInstance, LPTSTR szWindowClass)
+{
+	WNDCLASS wc;
+
+	wc.style         = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc   = WndProc;
+	wc.cbClsExtra    = 0;
+	wc.cbWndExtra    = 0;
+	wc.hInstance     = hInstance;
+	wc.hIcon         = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_J9W));
+	wc.hCursor       = 0;
+	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName  = 0;
+	wc.lpszClassName = szWindowClass;
+
+	return RegisterClass(&wc);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    HWND hWnd;
+    TCHAR szTitle[MAX_LOADSTRING];		// title bar text
+    TCHAR szWindowClass[MAX_LOADSTRING];	// main window class name
+
+    g_hInst = hInstance; // Store instance handle in our global variable
+
+    // SHInitExtraControls should be called once during your application's initialization to initialize any
+    // of the device specific controls such as CAPEDIT and SIPPREF.
+    SHInitExtraControls();
+
+    LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING); 
+    LoadString(hInstance, IDC_J9W, szWindowClass, MAX_LOADSTRING);
+
+    //If it is already running, then focus on the window, and exit
+    hWnd = FindWindow(szWindowClass, szTitle);	
+    if (hWnd) 
+    {
+        // set focus to foremost child window
+        // The "| 0x00000001" is used to bring any owned windows to the foreground and
+        // activate them.
+        SetForegroundWindow((HWND)((ULONG) hWnd | 0x00000001));
+        return 0;
+    } 
+
+    if (!MyRegisterClass(hInstance, szWindowClass))
+    {
+    	return FALSE;
+    }
+
+    hWnd = CreateWindow(szWindowClass, szTitle, WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    // When the main window is created using CW_USEDEFAULT the height of the menubar (if one
+    // is created is not taken into account). So we resize the window after creating it
+    // if a menubar is present
+    if (g_hWndMenuBar)
+    {
+        RECT rc;
+        RECT rcMenuBar;
+
+        GetWindowRect(hWnd, &rc);
+        GetWindowRect(g_hWndMenuBar, &rcMenuBar);
+        rc.bottom -= (rcMenuBar.bottom - rcMenuBar.top);
+		
+        MoveWindow(hWnd, rc.left, rc.top, rc.right-rc.left, rc.bottom-rc.top, FALSE);
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+
+    return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    int wmId, wmEvent;
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    static SHACTIVATEINFO s_sai;
+	
+    switch (message) 
+    {
+        case WM_COMMAND:
+            wmId    = LOWORD(wParam); 
+            wmEvent = HIWORD(wParam); 
+            // Parse the menu selections:
+            switch (wmId)
+            {
+                case IDM_HELP_ABOUT:
+                    DialogBox(g_hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, About);
+                    break;
+                case IDM_OK:
+                    SendMessage (hWnd, WM_CLOSE, 0, 0);				
+                    break;
+                default:
+                    return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+            break;
+        case WM_CREATE:
+            SHMENUBARINFO mbi;
+
+            memset(&mbi, 0, sizeof(SHMENUBARINFO));
+            mbi.cbSize     = sizeof(SHMENUBARINFO);
+            mbi.hwndParent = hWnd;
+            mbi.nToolBarId = IDR_MENU;
+            mbi.hInstRes   = g_hInst;
+
+            if (!SHCreateMenuBar(&mbi)) 
+            {
+                g_hWndMenuBar = NULL;
+            }
+            else
+            {
+                g_hWndMenuBar = mbi.hwndMB;
+            }
+
+            // Initialize the shell activate info structure
+            memset(&s_sai, 0, sizeof (s_sai));
+            s_sai.cbSize = sizeof (s_sai);
+			//start thread
+			hThread=CreateThread(NULL,0,watchdogthread,hWnd,0,&dwThreadID);
+			if(hThread)
+				DEBUGMSG(1,(L"thread created\n"));
+			else
+				DEBUGMSG(1,(L"thread create failed: 0x%08x\n",GetLastError()));
+            break;
+		
+		case WM_SHOWDIALOG:
+			DialogBox(g_hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, About);
+			break;
+
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            
+            // TODO: Add any drawing code here...
+            
+            EndPaint(hWnd, &ps);
+            break;
+        case WM_DESTROY:
+            CommandBar_Destroy(g_hWndMenuBar);
+            PostQuitMessage(0);
+            break;
+
+        case WM_ACTIVATE:
+            // Notify shell of our activate message
+            SHHandleWMActivate(hWnd, wParam, lParam, &s_sai, FALSE);
+            break;
+        case WM_SETTINGCHANGE:
+            SHHandleWMSettingChange(hWnd, wParam, lParam, &s_sai);
+            break;
+
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_INITDIALOG:
+            {
+                // Create a Done button and size it.  
+                SHINITDLGINFO shidi;
+                shidi.dwMask = SHIDIM_FLAGS;
+                shidi.dwFlags = SHIDIF_DONEBUTTON | SHIDIF_SIPDOWN | SHIDIF_SIZEDLGFULLSCREEN | SHIDIF_EMPTYMENU;
+                shidi.hDlg = hDlg;
+                SHInitDialog(&shidi);
+            }
+            return (INT_PTR)TRUE;
+
+        case WM_COMMAND:
+            if (LOWORD(wParam) == IDOK)
+            {
+                EndDialog(hDlg, LOWORD(wParam));
+                return TRUE;
+            }
+            break;
+
+        case WM_CLOSE:
+            EndDialog(hDlg, message);
+            return TRUE;
+
+    }
+    return (INT_PTR)FALSE;
+}
